3e743d6fc59682656cfe8f535ac56b2a5eea0c29
Surendra Somala <surendra@caltech.edu>
2010-05-23 00:41:16 +0000

    Updated refine meshes of tpv210-2d

--- dynamic/scecdynrup/tpv210-2d/tabulate_onfaultdata.py
+++ dynamic/scecdynrup/tpv210-2d/tabulate_onfaultdata.py
@@ -11,11 +11,11 @@
 #
 
 cell = "tri3"
-dx = 100
+dx = 200
 dt = 0.05
 
-outputRoot = "output/%s_%3dm" % (cell,dx)
-outdir = "scecfiles/%s_%3dm/" % (cell,dx)
+outputRoot = "output/%s_%3dm_%s" % (cell,dx,"refine")
+outdir = "scecfiles/%s_%3dm_%s/" % (cell,dx,"refine")
 
 import numpy
 import time
