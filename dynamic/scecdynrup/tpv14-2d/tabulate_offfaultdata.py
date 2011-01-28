178fd1d442c034fca627e02fb24267e327479bdb
Brad Aagaard <baagaard@usgs.gov>
2011-01-28 00:18:19 +0000

    Switched to HDF5 output. Added files for smooth gradient in cell size.

--- dynamic/scecdynrup/tpv14-2d/tabulate_offfaultdata.py
+++ dynamic/scecdynrup/tpv14-2d/tabulate_offfaultdata.py
@@ -10,78 +10,71 @@
 # ----------------------------------------------------------------------
 #
 
-cell = "quad4"
+sim = "tpv14"
+cell = "tri3"
 dx = 100
 dt = 0.05
 
-outputRoot = "output/%s_%3dm" % (cell,dx)
-outdir = "scecfiles/%s_%3dm/" % (cell,dx)
+inputRoot = "output/%s_%s_%3dm_gradient" % (sim, cell,dx)
+outputRoot = "scecfiles/%s_%s_%3dm" % (sim, cell,dx)
 
 # ----------------------------------------------------------------------
-
+import tables
 import numpy
 import time
 
-from pylith.utils.VTKDataReader import VTKDataReader
-
 # ----------------------------------------------------------------------
-timestamps = numpy.arange(50,12001,50)
-targets = numpy.array([[+3000.0, -2000.0, 0.0],
-                       [-3000.0, -2000.0, 0.0],
-                       [+3000.0, +2000.0, 0.0],
-                       [ -600.0, +2000.0, 0.0],
-                       [-4200.0, +2000.0, 0.0],
-                       [+3000.0, +5000.0, 0.0],
-                       [-1400.0, +5000.0, 0.0],
-                       [-5900.0, +5000.0, 0.0],
-                       [+3000.0, +8000.0, 0.0],
-                       [-2300.0, +8000.0, 0.0],
-                       [-7600.0, +8000.0, 0.0]])
+targets = numpy.array([[+3000.0, -2000.0],
+                       [-3000.0, -2000.0],
+                       [+3000.0, +2000.0],
+                       [ -600.0, +2000.0],
+                       [-4200.0, +2000.0],
+                       [+3000.0, +5000.0],
+                       [-1400.0, +5000.0],
+                       [-5900.0, +5000.0],
+                       [+3000.0, +8000.0],
+                       [-2300.0, +8000.0],
+                       [-7600.0, +8000.0]])
+targets[:,1] += 2000.0
 
-targets[:,1] = targets[:,1] + 2000
 
-reader = VTKDataReader()
-tolerance = 1.0e-6
-
-# Get vertices and find indices of target locations
-filename = "%s_t%05d.vtk" % (outputRoot,timestamps[0])
-data = reader.read(filename)
-
-vertices = numpy.array(data['vertices'])
+h5 = tables.openFile("%s.h5" % (inputRoot), 'r')
+vertices = h5.root.geometry.vertices[:]
 ntargets = targets.shape[0]
 indices = []
-indices1 = []
+tolerance = 1.0e-6
 for target in targets:
     dist = ( (vertices[:,0]-target[0])**2 + 
-             (vertices[:,1]-target[1])**2 +
-             (vertices[:,2]-target[2])**2 )**0.5
+             (vertices[:,1]-target[1])**2 )**0.5
     min = numpy.min(dist)
-    indices.append(numpy.where(dist <= min+tolerance)[0])
-    indices1.append(numpy.where(dist <= min+tolerance)[0][0])
+    indices.append(numpy.where(dist <= min+tolerance)[0][0])
 
-print "Indices", indices
-print "Coordinates of selected points:",vertices[indices1,:]
+print "Indices: ", indices
+print "Coordinates of selected points:\n",vertices[indices,:]
 
-# Extract values
-nsteps = timestamps.shape[0]
-disp = numpy.zeros((nsteps,ntargets,3))  # 3-D array (time, targets, components)
-vel = numpy.zeros((nsteps,ntargets,3))
-itime = 0
-for timestamp in timestamps:
-    filename = "%s_t%05d.vtk" % (outputRoot,timestamp)
-    data = reader.read(filename)
-    fields = data['vertex_fields']
-    disp[itime,0:ntargets,:] = fields['displacement'][indices1,:].squeeze()
-    vel[itime,0:ntargets,:] = fields['velocity'][indices1,:].squeeze()
-    itime += 1
+# Get datasets
+disp = h5.root.vertex_fields.displacement[:]
+vel = h5.root.vertex_fields.velocity[:]
 
+# BEGIN TEMPORARY
+#timeStamps =  h5.root.vertex_fields.time (not yet available)
+ntimesteps = disp.shape[0]
+timeStamps = numpy.arange(0, dt*ntimesteps, dt)
+# END TEMPORARY
+
+disp = disp[:,indices,:]
+vel = vel[:,indices,:]
+zero = numpy.zeros( (ntimesteps, 1), dtype=numpy.float64)
+
+h5.close()
 
 # Write data
 headerA = \
-    "# problem = TPV14-2D\n" + \
-    "# author = Surendra N. Somala\n" + \
+    "# problem = %s-2D\n" % sim.upper() + \
+    "# author = Brad Aagaard\n" + \
+    "# date = %s\n" % (time.asctime()) + \
     "# code = PyLith\n" + \
-    "# code_version = 1.5.1\n" + \
+    "# code_version = 1.5.2a (scecdynrup branch)\n" + \
     "# element_size = %s\n" % dx
 headerB = \
     "# Time series in 7 columns of E14.6:\n" + \
@@ -106,30 +99,26 @@ timeScale = 1000.0
 dip = 7.5
 body = targets[:,0] / lengthScale
 strike = (targets[:,1] - 2000) / lengthScale
-time = timestamps / timeScale
 
 for iloc in xrange(ntargets):
     pt = locName % (round(10*body[iloc]), 
                     round(10*strike[iloc]),
                     round(10*dip))
-    filename = "%sbody%s.dat" % (outdir, pt)
+    filename = "%s_body%s.dat" % (outputRoot, pt)
     fout = open(filename, 'w')
     fout.write(headerA)
     fout.write("# time_step = %14.6E\n" % dt)
-    fout.write("# num_timesteps = %8d\n" % nsteps)
+    fout.write("# num_timesteps = %8d\n" % ntimesteps)
     fout.write(locHeader % (body[iloc], 
-                             strike[iloc], 
-                             dip))
+                            strike[iloc], 
+                            dip))
     fout.write(headerB)
-    data = numpy.transpose((time,
+    data = numpy.transpose((timeStamps,
                             -disp[:,iloc,1],
                             -vel[:,iloc,1],
-                            +disp[:,iloc,2],
-                            +vel[:,iloc,2],
+                            zero,
+                            zero,
                             -disp[:,iloc,0],
                             -vel[:,iloc,0]))
     numpy.savetxt(fout, data, fmt='%14.6e')
     fout.close()
-
-
-
