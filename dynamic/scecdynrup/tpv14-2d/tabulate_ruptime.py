707b9ab7cc9cf8055e17b0c1f43320ad6ff8f2b3
Surendra Somala <surendra@caltech.edu>
2011-02-11 01:58:09 +0000

    Switched to criteria on slip rate instead of slip for calculating rupture time since we now have numerical damping

--- dynamic/scecdynrup/tpv14-2d/tabulate_ruptime.py
+++ dynamic/scecdynrup/tpv14-2d/tabulate_ruptime.py
@@ -52,7 +52,7 @@ for timestamp in timestamps:
     filename = "%s-main_fault_t%05d.vtk" % (outputRoot,timestamp)
     data = reader.read(filename)
     fields = data['vertex_fields']
-    slipRate = fields['slip'][:,:].squeeze()
+    slipRate = fields['slip_rate'][:,:].squeeze()
 
     # Compute magnitude of slip rate
     slipRateMag = (slipRate[:,0]**2 + slipRate[:,1]**2)**0.5
