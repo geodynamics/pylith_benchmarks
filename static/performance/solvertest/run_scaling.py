6ea3208aa467f5addd7aee2d346e155dead6db6e
Brad Aagaard <baagaard@usgs.gov>
2012-03-29 16:13:59 +0000

    Convert shell script to python script.

---
+++ static/performance/solvertest/run_scaling.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Python script to facilitate running scaling benchmarks using cluster
+# with PBS scheduler.
+
+import os
+import sys
+import subprocess
+
+if len(sys.argv) != 3:
+    raise ValueError("usage: run_scaling.py CELL NPROCS")
+cell = sys.argv[1]
+nprocs = int(sys.argv[2])
+
+if not cell in ["hex8", "tet4"]:
+    raise ValueError("Cell type (%s) must be 'hex8' or 'tet4'." % cell)
+if (nprocs % 2) != 0:
+    raise ValueError("Number of processors (%d) must be a power of 2." % nprocs)
+
+
+for d in ["output", "logs"]:
+  if not os.path.isdir(d):
+      os.mkdir(d)
+
+job = "%s_np%03d" % (cell, nprocs)
+
+pbsfile = os.environ['HOME'] + "/.pyre/pylithapp/pylithapp_pbs.cfg"
+pcfiles = "fieldsplit_mult.cfg custompc.cfg"
+args = pbsfile + " --job.name=%s --job.stdout=logs/%s.log" % (job, job)
+
+if nprocs < 8:
+    ppn = nprocs
+else:
+    ppn = 8
+
+cmd = "pylith %s.cfg %s.cfg " % (cell, job) + \
+    pcfiles + \
+    "--nodes=%d --scheduler.ppn=%d" % (nprocs, ppn) + \
+    args
+
+print cmd
+subprocess.check_call(cmd, shell=True)
