a4b88f960d7fba6591c3143f8f6a11d44409aef1
Charles Williams <C.Williams@gns.cri.nz>
2012-04-17 04:44:31 +0000

    More cleanup.

--- quasistatic/sceccrustdeform/savageprescott/parameters/hex8_lock40_tau0-2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pylith hex8.cfg fault_lock40.cfg fieldsplit.cfg hex8_lock40_tau0-2.cfg
--- quasistatic/sceccrustdeform/savageprescott/parameters/tet4_lock40_tau0-2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pylith tet4.cfg fault_lock40.cfg fieldsplit.cfg tet4_lock40_tau0-2.cfg
