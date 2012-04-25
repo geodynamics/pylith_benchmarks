5d5783e2b69d145ae3a3226daff205c30ba26837
Brad Aagaard <baagaard@usgs.gov>
2012-04-25 23:08:52 +0000

    Updated run script for multiple resolutions.

--- quasistatic/sceccrustdeform/savageprescott/run_sims.py
+++ quasistatic/sceccrustdeform/savageprescott/run_sims.py
@@ -6,17 +6,21 @@ import os
 import sys
 import subprocess
 
-if len(sys.argv) >= 3:
+if len(sys.argv) >= 4:
     cell = sys.argv[1]
-    nprocs = int(sys.argv[2])
+    res = sys.argv[2]
+    nprocs = int(sys.argv[3])
     batch = None
-    if len(sys.argv) == 4:
-        batch = sys.argv[3].upper()
+    if len(sys.argv) == 5:
+        batch = sys.argv[4].upper()
 else:
-    raise ValueError("usage: run_sims.py CELL NPROCS [BATCH_SYSTEM]")
+    raise ValueError("usage: run_sims.py CELL RES NPROCS [BATCH_SYSTEM]")
 
 if not cell in ["hex8", "tet4"]:
     raise ValueError("Cell type (%s) must be 'hex8' or 'tet4'." % cell)
+
+if not res in ["20km", "6.7km"]:
+    raise ValueError("Resolution (%s) must be '20km' or '6.7km'." % cell)
 
 if not batch is None and not batch in ["PBS"]:
     raise ValueError("Unknown batch system '%s'." % batch)
@@ -25,7 +29,7 @@ for d in ["output"]:
   if not os.path.isdir(d):
       os.mkdir(d)
 
-args = "%s.cfg fieldsplit.cfg --nodes=%d" % (cell, nprocs)
+args = "%s.cfg %s_%s.cfg fieldsplit.cfg --nodes=%d" % (cell, cell, res, nprocs)
 if batch == "PBS":
     pbsfile = os.environ['HOME'] + "/.pyre/pylithapp/pylithapp_pbs.cfg"
     args += " " + pbsfile + " --job.name=savageprescott_%s --job.stdout=%s.log" % (cell, cell)
