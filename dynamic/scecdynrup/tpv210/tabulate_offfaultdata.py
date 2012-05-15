3ea8699c10cecd76cb47d1546833adfc6902cc0b
Brad Aagaard <baagaard@usgs.gov>
2012-05-15 22:04:49 +0000

    Switched from tables to h5py.

--- dynamic/scecdynrup/tpv210/tabulate_offfaultdata.py
+++ dynamic/scecdynrup/tpv210/tabulate_offfaultdata.py
@@ -16,7 +16,7 @@ inputRoot = "output/%s_%s_%03dm" % (sim,cell,dx)
 outdir = "scecfiles/%s_%s_%03dm/" % (sim,cell,dx)
 
 # ----------------------------------------------------------------------
-import tables
+import h5py
 import numpy
 import time
 
@@ -33,8 +33,8 @@ targets = numpy.array([[-3000.0,      0.0,   -0.0],
 
 tolerance = 1.0e-6
 
-h5 = tables.openFile("%s.h5" % inputRoot, 'r')
-vertices = h5.root.geometry.vertices[:]
+h5 = h5py.File("%s.h5" % inputRoot, 'r', driver="sec2")
+vertices = h5['geometry/vertices'][:]
 ntargets = targets.shape[0]
 indices = []
 for target in targets:
@@ -49,9 +49,9 @@ print "Coordinates of selected points:\n",vertices[indices,:]
 
 
 # Get datasets
-disp = h5.root.vertex_fields.displacement[:]
-vel = h5.root.vertex_fields.velocity[:]
-timeStamps =  h5.root.time[:].ravel()
+disp = h5['vertex_fields/displacement'][:]
+vel = h5['vertex_fields/velocity'][:]
+timeStamps =  h5['time'][:].ravel()
 nsteps = timeStamps.shape[0]
 dt = timeStamps[1] - timeStamps[0]
 
--- dynamic/scecdynrup/tpv210/tabulate_ruptime.py
+++ dynamic/scecdynrup/tpv210/tabulate_ruptime.py
@@ -16,7 +16,7 @@ inputRoot = "output/%s_%s_%03dm-fault" % (sim,cell,dx)
 outdir = "scecfiles/%s_%s_%03dm/" % (sim,cell,dx)
 
 # ----------------------------------------------------------------------
-import tables
+import h5py
 import numpy
 import time
 
@@ -24,11 +24,11 @@ import time
 threshold = 0.001 # threshold for detecting slip has started
 maxTime = 1.0e+10 # Large value for default rupture time
 
-h5 = tables.openFile("%s.h5" % inputRoot, 'r')
-vertices = h5.root.geometry.vertices[:]
-slip = h5.root.vertex_fields.slip[:]
-slipRate = h5.root.vertex_fields.slip_rate[:]
-timeStamps =  h5.root.time[:].ravel()
+h5 = h5py.File("%s.h5" % inputRoot, 'r', driver="sec2")
+vertices = h5['geometry/vertices'][:]
+slip = h5['vertex_fields/slip'][:]
+slipRate = h5['vertex_fields/slip_rate'][:]
+timeStamps =  h5['time'][:].ravel()
 dt = timeStamps[1] - timeStamps[0]
 
 h5.close()
