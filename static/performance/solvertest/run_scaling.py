672214f1270f5a4060edb5b99d72cbf465bf3e05
Brad Aagaard <baagaard@usgs.gov>
2012-05-16 16:36:03 +0000

    More tweaks to scaling setup.

--- static/performance/solvertest/run_scaling.py
+++ static/performance/solvertest/run_scaling.py
@@ -43,7 +43,7 @@ if not "reduced" in pc:
 else:
     bcfiles = " bc_reduced.cfg nooutput.cfg "
 
-args = pbsfile + " --job.name=%s --job.stdout=logs/%s.log" % (job, job)
+args = pbsfile + " --job.name=%s --job.stdout=logs/%s.log --job.stdout=logs/%s.err" % (job, job, job)
 
 if nprocs < 8:
     ppn = nprocs
