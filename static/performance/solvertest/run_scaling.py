df43562007219ff476f00b5f728783a611336462
Brad Aagaard <baagaard@usgs.gov>
2012-08-24 19:48:13 +0000

    Updated scaling script for ppn=12 and lonestar.

--- static/performance/solvertest/run_scaling.py
+++ static/performance/solvertest/run_scaling.py
@@ -8,21 +8,18 @@ import os
 import sys
 import subprocess
 
-if len(sys.argv) != 7:
-    raise ValueError("usage: run_scaling.py PC CELL DOMAIN PPN NNODES QUEUE")
+if len(sys.argv) != 6:
+    raise ValueError("usage: run_scaling.py PC CELL PPN NNODES QUEUE")
 pc = sys.argv[1]
 cell = sys.argv[2]
-domain = sys.argv[3]
-ppn = int(sys.argv[4])
-nnodes = int(sys.argv[5])
-queue = sys.argv[6]
+ppn = int(sys.argv[3])
+nnodes = int(sys.argv[4])
+queue = sys.argv[5]
 
 if not pc in ["asm_reduced", "asm", "amg", "schur"]:
     raise ValueError("PC type (%s) must be 'asm_reduced', 'asm', 'amg', or 'schur'." % pc)
 if not cell in ["hex8", "tet4"]:
     raise ValueError("Cell type (%s) must be 'hex8' or 'tet4'." % cell)
-if not domain in ["original", "cube"]:
-    raise ValueError("Domain (%s) must be 'original' or 'cube'." % domain)
 
 nprocs = ppn*nnodes
 for d in ["output", "logs"]:
@@ -30,12 +27,8 @@ for d in ["output", "logs"]:
       os.mkdir(d)
 
 
-if domain == "cube":
-    job = "%s_%s_%s_np%03d" % (cell, domain, pc, nprocs)
-    mesh = "%s_%s_np%03d" % (cell, domain, nprocs)
-else:
-    job = "%s_%s_np%03d" % (cell, pc, nprocs)
-    mesh = "%s_np%03d" % (cell, nprocs)
+job = "%s_%s_np%03d" % (cell, pc, nprocs)
+mesh = "%s_np%03d" % (cell, nprocs)
 
 if queue.lower() != "none":
     batchfile = os.environ['HOME'] + "/.pyre/pylithapp/pylithapp_%s.cfg" % queue
@@ -76,4 +69,4 @@ elif queue == "lonestar":
 
 
 print cmd
-#subprocess.call(cmd, shell=True)
+subprocess.call(cmd, shell=True)
